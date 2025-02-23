c473cae92f293b0f8ba45209c1328f662a236d86
朱波 <zhubo0327@gmail.com>
2025-02-23 10:25:07 +0800

    feat: 主题

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import {RouterProvider} from "react-router-dom";
 import router from "./router";
+import {ConfigProvider, theme} from "antd";
+import {useStore} from "./store";
 
 function App() {
+    const {isDark} = useStore();
     return (
-        <RouterProvider router={router} />
+        <ConfigProvider theme={{
+            algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+            token: {
+                colorPrimary: "#1677ff"
+            }
+        }}>
+            <RouterProvider router={router} />
+        </ConfigProvider>
     );
 }
 
--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,11 +1,11 @@
 import styles from "./index.module.less";
-import {Button, Dropdown, MenuProps} from "antd";
+import {Button, Dropdown, MenuProps, Switch} from "antd";
 import {MenuFoldOutlined, MenuUnfoldOutlined} from "@ant-design/icons";
 import storage from "../../utils/storage.ts";
 import {useStore} from "../../store";
 
 function Header() {
-    const {collapsed, updateCollapsed} = useStore();
+    const {collapsed, updateCollapsed, isDark, updateTheme} = useStore();
     const items: MenuProps["items"] = [
         {
             key: "email",
@@ -17,8 +17,8 @@ function Header() {
         },
     ];
 
-    const onClick = ({key}: {key: string}) => {
-        if(key === "logout") {
+    const onClick = ({key}: { key: string }) => {
+        if (key === "logout") {
             storage.remove("token");
             location.href = "/login";
         }
@@ -27,7 +27,18 @@ function Header() {
     const toggleCollapsed = () => {
         updateCollapsed();
     };
-    
+
+    const handleSwitch = (isDark: boolean) => {
+        if (isDark) {
+            document.documentElement.dataset.theme = "dark";
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.dataset.theme = "light";
+            document.documentElement.classList.remove("dark");
+        }
+        updateTheme(isDark);
+    };
+
     return (
         <div className={styles.navHeader}>
             <div className={styles.left}>
@@ -45,7 +56,8 @@ function Header() {
                 </div>
             </div>
             <div className={styles.right}>
-                <Dropdown menu={{ items, onClick }} trigger={["click"]}>
+                <Switch checkedChildren="暗黑" unCheckedChildren="明亮" style={{marginRight: "20px"}} checked={isDark} onChange={handleSwitch} />
+                <Dropdown menu={{items, onClick}} trigger={["click"]}>
                     <span className={styles.nickName}>admin</span>
                 </Dropdown>
             </div>
